3d568ca9b90f0c2e0a9423c3f27b74bb9d28c330
giuseppe_galluzzi <galluzzigiuseppe99@gmail.com>
2021-11-03 16:54:57 +0100

    sistemazione dei file

--- src/App.js
+++ src/App.js
@@ -1,28 +1,7 @@
-import GameList from "./components/gameList/GameList";
 const App = () => {
-
-  let obj = [
-    {
-      name: 'Mario',
-      score: 0
-    },
-    {
-      name: 'Luisa',
-      score: 2
-    },
-    {
-      name: 'Gianni',
-      score: 5
-    },
-    {
-      name: 'Claudio',
-      score: 1
-    },
-  ]
 
   return (
     <div>
-      <h1>Prova</h1>
     </div>
   );
 }
